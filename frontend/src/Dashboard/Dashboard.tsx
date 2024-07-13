8d21cd2768323a0d4a7312fba48b1fbaef04ae00
Alex Kozik <ajk333@cornell.edu>
2024-07-13 19:08:57 -0400

    Worked on the FE

--- frontend/src/Dashboard/Dashboard.tsx
+++ frontend/src/Dashboard/Dashboard.tsx
@@ -3,13 +3,20 @@ import { DragHandleDots2Icon } from "@radix-ui/react-icons";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import "./Dashboard.css";
 import React from "react";
+import { Typography } from "@mui/material";
 
 
 const Events: React.FC = () => {
 
   return (
     <div className = "events">
-      <h1>Events</h1>
+      <Typography variant = "h4"
+      sx = {{
+        width: "fit-content",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+      >Events</Typography>
     </div>
   )
 }
@@ -17,7 +24,13 @@ const Events: React.FC = () => {
 const Tasks: React.FC = () => {
   return (
     <div className = "tasks">
-      <h1>Tasks</h1>
+      <Typography variant = "h4"
+      sx = {{
+        width: "fit-content",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+      >Tasks</Typography>
     </div>
   )
 }
@@ -26,9 +39,60 @@ const LeftBar: React.FC = () => {
   
   return (
     <div className = "left-bar">
-      <DateCalendar className = "calendar"/>
+      <DateCalendar className = "calendar"
+      sx = {{
+        marginBottom: "10px"
+      }}
+      />
       <Events/>
       <Tasks/>
+    </div>
+  )
+}
+
+const DayOfTheWeek = (props: {day: string}) => {
+  return (
+    <div className = "day-of-the-week">
+      <Typography variant = "h5"
+      sx = {{
+        width: "fit-content",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+      >{props.day}</Typography>
+    </div>
+  )
+}
+
+const CalendarHeader: React.FC = () => {
+  // display the current month and year
+  return (
+    <div className = "calendar-header">
+      <Typography variant = "h4"
+      sx = {{
+        width: "fit-content",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+      >August 2021</Typography>
+    </div>
+  )
+}
+
+const WeeklyCalendar: React.FC = () => {
+  return (
+    <div className = "weekly-calendar">
+      <CalendarHeader/>
+
+      <div className = "weekly-calendar-days">
+        <DayOfTheWeek day = "Mo"/>
+        <DayOfTheWeek day = "Tu"/>
+        <DayOfTheWeek day = "We"/>
+        <DayOfTheWeek day = "Th"/>
+        <DayOfTheWeek day = "Fr"/>
+        <DayOfTheWeek day = "Sa"/>
+        <DayOfTheWeek day = "Su"/>
+      </div>
     </div>
   )
 }
@@ -37,13 +101,13 @@ const Dashboard: React.FC = () => {
   return (
     <PanelGroup autoSaveId="example" direction="horizontal">
       <Panel defaultSize = {25} minSize = {10} maxSize = {30}>
-        <LeftBar/>F
+        <LeftBar/>
       </Panel>
 
       <PanelResizeHandle className = "resize-handle">
       </PanelResizeHandle>
       <Panel defaultSize = {75}>
-        Right section
+      <WeeklyCalendar/>
       </Panel>
 
 
